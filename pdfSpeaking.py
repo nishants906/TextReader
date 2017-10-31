9c020740af0a88ebeccfc7e6cf6d2cab272b1301
Nishant Sardana <sardana.nishant@gmail.com>
2017-10-31 21:21:59 +0530

    speaker added

--- pdfSpeaking.py
+++ pdfSpeaking.py
@@ -1,1 +1,11 @@
-importhttps://github.com/sardananishant/TextReader.git
+import pyttsx
+
+
+def fileSpeaker(file):
+
+    speaker = pyttsx.init()
+    speaker.setProperty('rate',150)
+    speaker.setProperty('voice','en+m7')
+    speaker.say(file)
+    speaker.runAndWait()
+
